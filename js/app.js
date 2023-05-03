cac2fbf55c8c1aabf770e6e5a1b483f2417a64fb
keviquoc <keviinqiceno@gmail.com>
2023-05-03 12:31:56 -0500

    Version Final Node

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 function saludo() {
     var Saludomsj = "Hola, te queremos enviar un saludo y dar las gracias por visitarnos, te esperamos pronto en una de nuestras sedes";
 
-    alert(Saludomsj);
+    document.getElementById('saludoModal').innerHTML = Saludomsj;
 
 }
